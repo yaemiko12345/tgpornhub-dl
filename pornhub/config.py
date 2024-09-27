892e0fe9a94bf1c148fcbbded8a68ce8607b621a
yaemiko12345 <vaviloid98+1@gmail.com>
2024-09-27 11:50:15 +0700

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -2,7 +2,7 @@ from typing import List
 
 API_ID: int = 12345
 API_HASH: str = "abcdefg:12345678"
-TOKEN: str = "abcdefg:12345678"
+TOKEN: str = "7656527709:AAEDaSgfU2fXoc7aZ_Y6R219LK6QNd_ycrI"
 
 log_chat: int = -12345678
 sub_chat: str = "channel_username"
